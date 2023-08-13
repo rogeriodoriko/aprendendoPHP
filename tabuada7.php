ccccab9cbff35071744f84492ac25cf38ec648bb
Rogério Fernandes <112347141+rogeriodoriko@users.noreply.github.com>
2023-08-12 21:28:55 -0300

    Add files via upload

---
+++ tabuada7.php
@@ -0,0 +1,25 @@
+<?php 
+echo "<i><h2>Tabuada do 7</h2></i>";
+$result = 7*1;
+echo "7x1 = $result <br>";
+$result = 7*2;
+echo "7x2 = $result <br>";
+$result = 7*3;
+echo "7x3 = $result <br>";
+$result = 7*4;
+echo "7x4 = $result <br>";
+$result = 7*5;
+echo "7x5 = $result <br>";
+$result = 7*6;
+echo "7x6 = $result <br>";
+$result = 7*7;
+echo "7x7 = $result <br>";
+$result = 7*8;
+echo "7x8 = $result <br>";
+$result = 7*9;
+echo "7x9 = $result <br>";
+$result = 7*10;
+echo "7x10 = $result <br>";
+
+
+
